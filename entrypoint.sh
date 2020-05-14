b781814507c2c312cfcfde8dd1f1da86200bca86
Shawn Wheeler <swheeler@g2.com>
2020-05-14 17:55:55 -0500

    PARR-82 output try 4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "::set-output name=issueDetails::$(curl -s -u \"$1:$2\" https://$3.atlassian.net/rest/api/2/issue/$4)"
+details=`curl -s -u "$1:$2" https://$3.atlassian.net/rest/api/2/issue/$4`
+echo "::set-output name=issueDetails::$details"
+# echo "::set-output name=issueDetails::$(curl -s -u \"$1:$2\" https://$3.atlassian.net/rest/api/2/issue/$4)"
