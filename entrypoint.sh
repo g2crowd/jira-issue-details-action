e1829ad04de91791d53e31df8c9c7544afb3db20
Shawn Wheeler <swheeler@g2.com>
2020-05-15 09:42:49 -0500

    PARR-82 output with jq 3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 details=`curl -s -u "$1:$2" https://$3.atlassian.net/rest/api/2/issue/$4`
-echo "::set-output name=issueDetails::$details | jq -c .fields.labels"
-# echo "::set-output name=issueDetails::$(curl -s -u \"$1:$2\" https://$3.atlassian.net/rest/api/2/issue/$4)"
+echo "::set-output name=issueDetails::$details"
