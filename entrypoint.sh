759b089a24912808b57eb1dc2e281c2314d9b18b
Shawn Wheeler <swheeler@g2.com>
2020-05-15 15:14:29 -0500

    PARR-82 new docker image 3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-details=$(curl -s -u "$1:$2" https://$3.atlassian.net/rest/api/2/issue/$4)
+details=$(curl -s -u "$1:$2" https://$3.atlassian.net/rest/api/2/issue/$4 | jq -c .fields.labels)
 echo "::set-output name=issueDetails::$details"
