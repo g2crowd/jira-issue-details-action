8bb33bfb26e9770b891551e31d628f34e9888027
Shawn Wheeler <swheeler@g2.com>
2020-05-15 10:29:17 -0500

    PARR-82 try with jq

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-details=`curl -s -u "$1:$2" https://$3.atlassian.net/rest/api/2/issue/$4 | jq -c ".fields.labels"`
+# details=`curl -s -u "$1:$2" https://$3.atlassian.net/rest/api/2/issue/$4`
+details=$(curl -s -u "$1:$2" https://$3.atlassian.net/rest/api/2/issue/$4 | jq -c .fields.labels)
 echo "::set-output name=issueDetails::$details"
+
